9b82da7bfdd8acb190f6f7b4a108f6cbe076a6d4
khaledboussaidi <khaled.boussaidi@etudiant-enit.utm.tn>
2020-10-28 17:00:35 +0100

    first one

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const url = 'mongodb://localhost:27017/myindex'
+
+const app = express()
+
+mongoose.connect(url,{useNewUrlParser:true})
+const con = mongoose.connection
+
+con.on('open',function(){
+    console.log('database opened')
+})
+const myrouter = require('../mongdb/contoller')
+//app.use(bodyParser);
+app.use(express.json())
+app.use('/',myrouter)
+app.listen(9090,()=> {console.log('hello from node server ...')})
